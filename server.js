ae0da37d501adab47173e9f866f05ebc0e27d717
John Wiebalk <john.wiebalk@me.com>
2019-03-27 18:17:10 +0000

    remove octokit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http')
 var createHandler = require('github-webhook-handler')
 var handler = createHandler({ path: '/webhook', secret: (process.env.SECRET)})
+var impersonationToken = ""
 
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
@@ -20,12 +21,55 @@ handler.on('organization', function (event) {
   }
 })
 
-// handler.on('*', function (emitData) {
-//   console.log(prettyjson.render(emitData, options))
-//  })
-
 function addAdmins(org, creator)
 {
   console.log('Received an organization event for %s from %s',
     org, creator)
+  getImpersonation(creator)
+
+}
+
+function getImpersonation(creator)
+{
+
+const https = require('https')
+const data = JSON.stringify({
+  scopes: ["admin:org"]
+})
+
+const options = {
+  hostname: (process.env.GHE_HOST),
+  port: 443,
+  path: '/api/v3/admin/users/' + creator + "/authorizations",
+  method: 'POST',
+  headers: {
+    'Authorization': 'token ' + (process.env.GHE_TOKEN),
+    'Content-Type': 'application/json',
+    'Content-Length': data.length
+  }
+}
+let body = [];
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+  res.on('data', (chunk) => {
+        body.push(chunk);
+      }).on('end', () => {
+        body = Buffer.concat(body).toString();
+  // at this point, `body` has the entire request body stored in it as a string
+    parseJSON(body)
+    })
+
+})
+
+req.on('error', (error) => {
+  console.error(error)
+})
+
+ req.write(data)
+req.end()
+
+}
+
+function parseJSON(body) {
+  impersonationToken = JSON.parse(body).token
 }
